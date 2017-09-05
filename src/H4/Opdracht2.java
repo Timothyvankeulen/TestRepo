9d38d4d23baeb338f10bd93e2f6f983f19eb1a86
Timothy van Keulen <Maildx4@gmail.com>
2017-09-05 14:38:31 +0200

    Added Opdracht2 Class

---
+++ src/H4/Opdracht2.java
@@ -0,0 +1,26 @@
+package H4;
+
+import java.awt.*;
+import java.applet.*;
+
+public class Opdracht2 extends Applet {
+
+    public void init() {
+    }
+
+    public void paint(Graphics g){
+        setBackground(Color.white);
+        g.setColor(Color.blue);
+        //Het dak van het huis
+        g.drawLine(50,100,150,100);
+        g.drawLine(100,50,150,100);
+        g.drawLine(100,50,50,100);
+        //Het blok van het huis
+        g.drawRect(50,100,100,100);
+        //Het raam in het huis
+        g.drawRect(60,115,25,25);
+        //De deur in het huis
+        g.drawRect(120,160,15,40);
+        g.drawOval(128,175,4,4);
+    }
+}
